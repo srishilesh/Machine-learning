a3a947ab6539fec9a9019385bb64b108f2acf473
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-01-20 22:38:21 +0530

    Create Train_test_split.py

---
+++ Datapreprocessing/Train_test_split.py
@@ -0,0 +1,17 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv(file)
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+print(X_train)
