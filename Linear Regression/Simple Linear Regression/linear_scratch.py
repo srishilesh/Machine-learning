7a9984afac973af7c0434db93a29ffc3792a217c
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-05-20 23:09:45 +0530

    Update linear_scratch.py

--- Linear Regression/Simple Linear Regression/linear_scratch.py
+++ Linear Regression/Simple Linear Regression/linear_scratch.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+from numpy.linalg import matrix_power
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('E:/General/Courses/Machine learning- Coursera/Assignment/Ex 1/machine-learning-ex1/ex1/ex1data1.txt',names=['Pop','Profit'])
+df = np.loadtxt('E:/General/Courses/Machine learning- Coursera/Assignment/Ex 1/machine-learning-ex1/ex1/ex1data1.txt', delimiter=',')
 
 #print(df.head())
 
@@ -13,12 +14,42 @@ plt.show()
 '''
 #df.insert(0,'Ones',1)       # To insert a new column of ones for matrix multiplication
 
-#cols = df.shape[1]
-X = df.iloc[:,:cols-1]          # To split the training set
-y = df.iloc[:,cols-1:cols]      # To split the target variable
+cols = df.shape[1]
+X = df[:,0]          # To split the training set
+y = df[:,1]      # To split the target variable
 m = y.size                      # Number of training examples
 
 def plotData(X,y):
     plt.plot(X,y,'ro',ms = 10,mec = 'k')
     plt.ylabel("Profit in $10,000")
     plt.xlabel("Pop. of city in 10,000s")
+    plt.show()
+
+
+
+def computeCost(X,y,theta):
+    m = y.size  # No of features
+    J =  0      # Cost computed
+    #for i in range(1,m+1):
+    h = X.dot(theta)
+    J = (1/(2*m))*(np.sum(np.square(h-y)))
+    return J
+
+X = np.stack([np.ones(m),X],axis=1)     # Adding an additional column of ones to the X matrix for easier manipulation
+
+
+J = computeCost(X, y, theta=np.array([0.0, 0.0]))
+print('With theta = [0, 0] \nCost computed = %.2f' % J)
+print('Expected cost value (approximately) 32.07\n')
+#print(J)
+
+# further testing of the cost function
+J = computeCost(X, y, theta=np.array([-1, 2]))
+print('With theta = [-1, 2]\nCost computed = %.2f' % J)
+print('Expected cost value (approximately) 54.24')
+
+
+#plotData(X,y)       # Plotting the data in the graph
+print(X.shape)
+theta=np.array([0.0, 0.0])
+print(theta.shape)
